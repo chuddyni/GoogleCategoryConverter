dd2c0b92bec8b72205d3b842b8e74019688b2092
chuddyni <chuddyni@gmail.com>
2019-07-17 11:49:16 +0200

    adding tests

--- app.js
+++ app.js
@@ -6,24 +6,22 @@ class GoogleCategoryConverter {
     try {
       this.data = require(locales[Object.keys(locales)[0]]);
     }
-    catch(e) {
-      console.log("bladblad blad");
-      
-      console.log(e);
+    catch (e) {
     }
   }
 
   getCategories(categoryNumber, level) {
     // var obj = JSON.parse(fs.readFileSync(locales[Object.keys(locales)[0]], 'utf8'));
     // return obj[categoryNumber];
-    var dataLength = this.data[categoryNumber].length; 
-    if (level){
-      //sprawdzenie czy oczekiwania tablica jest mniejsza od rzeczywistej tablicy
-      if(level.level < dataLength){
-        return this.data[categoryNumber].slice(level.level,dataLength);
+    try {
+      var dataLength = this.data[categoryNumber].length;
+      if (level && level.level < dataLength) {
+        //sprawdzenie czy oczekiwania tablica jest mniejsza od rzeczywistej tablicy
+        return this.data[categoryNumber].slice(level.level, dataLength);
+      } else {
+        return this.data[categoryNumber];
       }
-    }else{
-      return this.data[categoryNumber];
+    } catch (e) {
     }
   }
 }
@@ -41,7 +39,7 @@ const converter = new GoogleCategoryConverter(locales);
 
 // console.log(converter.getCategories(3578));
 
-// module.exports = GoogleCategoryConverter;
-console.log(converter.getCategories(3578, {level:"1"}));
+module.exports = GoogleCategoryConverter;
+// console.log(converter.getCategories(141, { level: "3" }));
 
 // console.log(Object.keys(locales)[0]);
--- test/appTest.js
+++ test/appTest.js
@@ -1,19 +1,49 @@
 var app = require('../app');
 var assert = require('assert');
+var should = require('should')
 
-var locales = { "pl_PL":  "../taxonomy.json" };
+var locales = { "pl_PL": "../taxonomy.json" };
 
 var converter = new app(locales);
 
 // console.log(converter.getCategories(3578));
 
 
-describe('Converter', function() {
-  describe('#indexOf()', function() {
-    it('dla 3578 powinien zwrocic tablice 4 elementow', function(){
-        var categories = converter.getCategories(3578);
-        var expectatios = ['Sprzęt sportowy','Rekreacja na świeżym powietrzu','Golf','Chorągiewki golfowe'];          
-        assert.equal(categories.length,expectatios.length)
+describe('Converter', function () {
+    describe('#testy()', function () {
+        it('dla 3578 powinien zwrocic tablice 4 elementow', function () {
+            var categories = converter.getCategories(3578);
+            var expectatios = 4;
+            assert.equal(categories.length, expectatios)
+        });
+        it('dla 3578 powinien zwrocic tablice 2 elementow po podaniu level 2', function () {
+            var categories = converter.getCategories(3578, { level: "2" });
+            var expectatios = 2;
+            assert.equal(categories.length, expectatios)
+        });
+        it('dla 3578 powinien zwrocic tablice 4 elementow po podaniu level 5 (Wieksze od rzeczywistego rozmiaru)', function () {
+            var categories = converter.getCategories(3578, { level: "5" }).length;
+            var expectatios = 4;
+            assert.equal(categories, expectatios)
+        });
+        it('dla klucza nie obecnego w JSON powinien wyrzucic error', function (done) {
+            try {
+                var categories = converter.getCategories(123123123, { level: "5" }).length;
+                expect(categories).to.throw(Error);
+            }
+            catch (error) {
+                done();
+            }
+        });
+        it('dla klucza nie obecnego w JSON powinien wyrzucic error', function (done) {
+            try {
+                var locales = { "pl_PL": "../ASDbadNAME.json" };
+                var converter = new app(locales);
+                expect(converter).to.throw(Error);
+            }
+            catch (error) {
+                done();
+            }
+        });
     });
-  });
 });
